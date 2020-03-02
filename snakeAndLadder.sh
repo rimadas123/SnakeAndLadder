c7af547d06b174ed368c7935d451dec18d3dac36
rimadas123 <rdas5969@gmail.com>
2020-03-02 11:32:35 +0530

    Added : Repeat till the player reaches the winning position 100

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,17 +5,25 @@ echo "Welcome to Snake And Ladder Game"
 player=1
 position=0
 
+#for generating dice roll
 calRandom=$(( RANDOM%6 + 1 ))
 echo $calRandom
 
+#repeat till the winning player reaches 100
+while [[ position -le 100 ]]
+do
+#for checking the options like no play, snake or ladder
 option=$(( RANDOM%3 + 1 ))
 
+#no play
  if [[ $option -eq 1 ]]
  then
   position=$position
+#ladder
  elif [[ $option -eq 2 ]]
  then
   position=$(( $position+$calRandom ))
+#snake
  elif [[ $option -eq 3 ]]
  then
   position=$(( $position-$calRandom ))
@@ -23,5 +31,7 @@ option=$(( RANDOM%3 + 1 ))
   echo "wrong input"
  fi
 
+done
 
 echo $position
+
