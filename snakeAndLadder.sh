6eaa87135e67a3c96a03a82f688ac2bce9e57cd0
rimadas123 <rdas5969@gmail.com>
2020-03-02 10:35:46 +0530

    Added : The Player checks for option like no play, snake or ladder

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,5 +2,26 @@
 
 echo "Welcome to Snake And Ladder Game"
 
-player=0
+player=1
+position=0
 
+calRandom=$(( RANDOM%6 + 1 ))
+echo $calRandom
+
+option=$(( RANDOM%3 + 1 ))
+
+ if [[ $option -eq 1 ]]
+ then
+  position=$position
+ elif [[ $option -eq 2 ]]
+ then
+  position=$(( $position+$calRandom ))
+ elif [[ $option -eq 3 ]]
+ then
+  position=$(( $position-$calRandom ))
+ else
+  echo "wrong input"
+ fi
+
+
+echo $position
