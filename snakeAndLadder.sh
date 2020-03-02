0536ad919a2b82539e9d23d7caf855385ddff20d
rimadas123 <rdas5969@gmail.com>
2020-03-02 12:54:45 +0530

    Added : Ensure the player gets to exact winning position 100

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,6 +2,10 @@
 
 echo "Welcome to Snake And Ladder Game"
 
+#constants
+GOAL=100
+
+#variables
 player=1
 position=0
 
@@ -9,4 +13,26 @@ position=0
 calRandom=$(( RANDOM%6 + 1 ))
 echo $calRandom
 
-
+#checks if the position reached till 100
+while (( position <= $GOAL ))
+do
+	#for checking the options like no play, snake or ladder
+	option=$(( RANDOM%3 + 1 ))
+		#no play
+		if [[ $option -eq 1 ]]
+		then
+			position=$position
+		#ladder
+		elif [[ $option -eq 2 ]]
+		then
+			echo "random num->" $calRandom
+			position=$(( $position+$calRandom ))
+		#snake
+		elif [[ $option -eq 3 ]]
+		then
+			position=$(( $position-$calRandom ))
+		else
+		echo "Wrong input"
+		fi
+done
+echo "Player has reached the winning goal: " $position
