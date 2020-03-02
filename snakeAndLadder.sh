ad68750e437c71567cd0e6c5f8ec57868cb768e6
rimadas123 <rdas5969@gmail.com>
2020-03-02 11:39:43 +0530

    Added : The Player rolls the dice to get number between 1 to 6

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -9,29 +9,4 @@ position=0
 calRandom=$(( RANDOM%6 + 1 ))
 echo $calRandom
 
-#repeat till the winning player reaches 100
-while [[ position -le 100 ]]
-do
-#for checking the options like no play, snake or ladder
-option=$(( RANDOM%3 + 1 ))
-
-#no play
- if [[ $option -eq 1 ]]
- then
-  position=$position
-#ladder
- elif [[ $option -eq 2 ]]
- then
-  position=$(( $position+$calRandom ))
-#snake
- elif [[ $option -eq 3 ]]
- then
-  position=$(( $position-$calRandom ))
- else
-  echo "wrong input"
- fi
-
-done
-
-echo $position
 
